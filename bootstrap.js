3d5b2d1a6326e0203421cf08819f90e38b7e18fa
Anna <anna.gritsay@gmail.com>
2019-03-28 22:19:11 -0700

    made rows and columns, add a class to an image in js

--- bootstrap.js
+++ bootstrap.js
@@ -1,39 +1,43 @@
 function loadMoviesFromServer() {
   return [
     { title: "Ghostbusters",
-      image: "logo.png"
+      image: "Romantic.png"
     },
     { title: "Captain Marvel",
-      image: "poster2.jpg"
+      image: "Alita.png"
     },
     { title: "Shawshank Redemption",
-      image: "poster3.jpg"
+      image: "captainMarvel.png"
     },
     { title: "Sex In The City",
-      image: "poster4.jpg"
+      image: "WhatMenWant.png"
     }];;
 }
 
 function addMovies() {
+  var moviesArray = loadMoviesFromServer();
 
-  var movies = loadMoviesFromServer();
+  for(var i=0; i<moviesArray.length; i++) {
+    addOneMovieHtml(moviesArray[i]);
+  }
+}
 
+function addOneMovieHtml(movie) {
   var container = document.getElementById("nowShowingSlider");
 
-  for(var i=0; i<movies.length; i++) {
-    var divLevel1 = document.createElement("div");
-    divLevel1.className = "movieSliderMovie col-sm-3 col-md-2";
+  var divLevel1 = document.createElement("div");
+  divLevel1.className = "col-xs-3";
 
-    var newImg = document.createElement("img");
-    newImg.src = movies[i].image;
+  var newImg = document.createElement("img");
+  newImg.src = movie.image;
+  newImg.className = "img-responsive";
 
-    divLevel1.appendChild(newImg);
+  divLevel1.appendChild(newImg);
 
-    var p = document.createElement("p");
-    var node = document.createTextNode(movies[i].title);
-    p.appendChild(node);
-    divLevel1.appendChild(p);
+  var p = document.createElement("p");
+  var node = document.createTextNode(movie.title);
+  p.appendChild(node);
+  divLevel1.appendChild(p);
 
-    container.appendChild(divLevel1);
-  }
+  container.appendChild(divLevel1);
 }
